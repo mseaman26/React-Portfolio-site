e6fae2386fcee589fd529dfd578469daf49b5f61
michael seaman <mseaman26@gmail.com>
2022-12-04 21:50:06 -0800

    footer first draft

--- portfolio-site/src/components/Footer.js
+++ portfolio-site/src/components/Footer.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 function Footer(){
     return(
-        <h1>
-            footer
-        </h1>
+        <div id='footer_div'>
+            <a href='https://github.com/mseaman26' target={"blank"}>
+                Visit my GitHub Page
+            </a>
+            <a href='https://www.linkedin.com/in/michael-seaman-120a59250/' target={"blank"}>
+                Visit my LinkedIn
+            </a>
+            <a href='https://medium.com/@mseaman26' target={"blank"}>
+                View by Medium Blog posts
+            </a>
+            
+        </div>
     )
 }
 export default Footer
