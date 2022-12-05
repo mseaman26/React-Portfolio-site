54f60f4218a80b2629b31fa42a754a093dbb48e2
michael seaman <mseaman26@gmail.com>
2022-12-05 00:15:00 -0800

    styling

--- portfolio-site/src/components/Footer.js
+++ portfolio-site/src/components/Footer.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 function Footer(){
     return(
-        <div id='footer_div'>
-            <a href='https://github.com/mseaman26' target={"blank"}>
+        <div className='nav-links'>
+            <a href='https://github.com/mseaman26' target={"blank"} className='nav_link' >
                 <p>Visit my GitHub Page</p>
             </a>
-            <a href='https://www.linkedin.com/in/michael-seaman-120a59250/' target={"blank"}>
+            <a href='https://www.linkedin.com/in/michael-seaman-120a59250/' target={"blank"} className='nav_link'>
                 <p>Visit my LinkedIn</p>
             </a>
-            <a href='https://medium.com/@mseaman26' target={"blank"}>
+            <a href='https://medium.com/@mseaman26' target={"blank"} className='nav_link'>
                 <p>View my Medium Blog posts</p>
             </a>
             
