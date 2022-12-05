aae844eac32a28c9f34138fb7a1992bd19dcb09e
michael seaman <mseaman26@gmail.com>
2022-12-04 22:10:15 -0800

    footer styling

--- portfolio-site/src/components/Footer.js
+++ portfolio-site/src/components/Footer.js
@@ -3,13 +3,13 @@ function Footer(){
     return(
         <div id='footer_div'>
             <a href='https://github.com/mseaman26' target={"blank"}>
-                Visit my GitHub Page
+                <p>Visit my GitHub Page</p>
             </a>
             <a href='https://www.linkedin.com/in/michael-seaman-120a59250/' target={"blank"}>
-                Visit my LinkedIn
+                <p>Visit my LinkedIn</p>
             </a>
             <a href='https://medium.com/@mseaman26' target={"blank"}>
-                View by Medium Blog posts
+                <p>View my Medium Blog posts</p>
             </a>
             
         </div>
